3e9507e1a0f4c5781131c698849f08ce6ce23158
Nico-dsa <6463@holbertonstudents.com>
2023-05-14 10:19:10 -0700

    Add nop function

---
+++ _nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything
+ * @stack: double pointer
+ * @line_number: line number of opcode
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
--- get_func.c
+++ get_func.c
@@ -18,6 +18,7 @@ void get_func(char *opcode, stack_t **stack, unsigned int line_number)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL},
 	};
 	int i;
