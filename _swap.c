d8a840373187aa79af95236947b8364cfe362326
Nico-dsa <6463@holbertonstudents.com>
2023-05-14 09:56:07 -0700

    Add swap function

---
+++ _swap.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/** _swap - swaps the top two elements of the stack
+ *
+ * @stack: double pointer
+ * @line_number: line number of opcode
+ */
+
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	int tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
--- monty.h
+++ monty.h
@@ -44,5 +44,6 @@ int _isdigit(const char *str);
 void _push(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
 
 #endif
